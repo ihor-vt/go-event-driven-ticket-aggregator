fe1f1f3cbcd32dc2c7d8e073210cf40ea68fb5f7
Ihor Voitiuk <ihorvoitiukk@gmail.com>
2026-01-04 12:20:20 +0100

    feat: Add check how much booked tockets

--- db/booking_repository.go
+++ db/booking_repository.go
@@ -4,8 +4,10 @@ import (
 	"context"
 	"database/sql"
 	"fmt"
+	"net/http"
 
 	"github.com/jmoiron/sqlx"
+	"github.com/labstack/echo/v4"
 
 	"tickets/entities"
 	"tickets/message/event"
@@ -30,6 +32,36 @@ func (b BookingsRepository) AddBooking(ctx context.Context, booking entities.Boo
 		b.db,
 		sql.LevelRepeatableRead,
 		func(ctx context.Context, tx *sqlx.Tx) error {
+			availableSeats := 0
+			err = tx.GetContext(ctx, &availableSeats, `
+				SELECT
+					number_of_tickets AS available_seats
+				FROM
+					shows
+				WHERE
+					show_id = $1
+			`, booking.ShowID)
+			if err != nil {
+				return fmt.Errorf("could not get available seats: %w", err)
+			}
+
+			alreadyBookedSeats := 0
+			err = tx.GetContext(ctx, &alreadyBookedSeats, `
+				SELECT
+					COALESCE(SUM(number_of_tickets), 0) AS already_booked_seats
+				FROM
+					bookings
+				WHERE
+					show_id = $1
+			`, booking.ShowID)
+			if err != nil {
+				return fmt.Errorf("could not get already booked seats: %w", err)
+			}
+
+			if availableSeats-alreadyBookedSeats < booking.NumberOfTickets {
+				return echo.NewHTTPError(http.StatusBadRequest, "not enough seats available")
+			}
+
 			_, err = tx.NamedExecContext(ctx, `
 				INSERT INTO
 					bookings (booking_id, show_id, number_of_tickets, customer_email)
